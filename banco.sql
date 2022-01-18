01a58d9b00e04b16ace7a2f6c2679e7a53e09505
Anthony Santos <anthonygabriel642@gmail.com>
2022-01-18 20:00:13 -0300

    Criação da estrutura do banco

---
+++ banco.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE Banco;
+
+USE Banco;
+
+CREATE TABLE Usuario
+(
+  UserId INT IDENTITY(1,1) NOT NULL,
+  UserName VARCHAR(50),
+  UserEmail VARCHAR(50),
+  UserPassword VARCHAR(50),
+  CONSTRAINT pk_UsuarioId PRIMARY KEY (UserId)
+);
+
+INSERT INTO Usuario (UserName, UserEmail, UserPassword) VALUES ('Anthony Santos', 'anthonygabriel642@gmail.com', 'agas124578');
+INSERT INTO Usuario (UserName, UserEmail, UserPassword) VALUES ('Isadora Freitas', 'isadorafreitas@gmail.com', 'algoestranho');
+
+SELECT * FROM Usuario;
